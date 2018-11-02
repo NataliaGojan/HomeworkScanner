db0296347fe3b9b3b8948149bcfbd1c8177d651c
NataliaGojan <43417844+NataliaGojan@users.noreply.github.com>
2018-11-02 23:23:48 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -4,7 +4,7 @@ public class Triangle {
 
     public double side1, side2, side3;
 
-    public Triangle(double side1, double side2, double side3) {   //constructor
+    public Triangle(double side1, double side2, double side3) {   
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
@@ -15,7 +15,7 @@ public class Triangle {
     }
 
     double getArea() {
-        double sp = getPerimeter() / 2;                //sp = semiperimeter
+        double sp = getPerimeter() / 2;              
 
         double area = (Math.sqrt(sp * (sp - side1) * (sp - side2)) * (sp - side3));
         return area;
